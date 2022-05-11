a45f02d35bddf549d687dd42524bcf503dd9b3f3
rijan adhikari <rijanadhikari@gmail.com>
2022-05-11 10:41:39 +0900

    fixed imports

--- src/flask_classful_apispec/__init__.py
+++ src/flask_classful_apispec/__init__.py
@@ -1,5 +1,8 @@
 import pkg_resources
+from .api_spec import APISpec
+from .flask_classful_plugin import FlaskClassfulPlugin
 
 __version__ = str(
     pkg_resources.get_distribution("flask-classful-apispec").parsed_version
 )
+__all__ = ['APISpec', 'FlaskClassfulPlugin']
